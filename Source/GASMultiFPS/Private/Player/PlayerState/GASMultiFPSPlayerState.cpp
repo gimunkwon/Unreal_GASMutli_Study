6c903bfca0fc4b3c226c714074ba8b5d0b3bf8f1
gimunkwon <gimon71@gmail.com>
2026-06-04 23:02:46 +0900

    [Add,Feature] ASC 게터 함수 추가 어빌리티 시스템 인터페이스 상속및 순수 가상 함수 구현

--- Source/GASMultiFPS/Private/Player/PlayerState/GASMultiFPSPlayerState.cpp
+++ Source/GASMultiFPS/Private/Player/PlayerState/GASMultiFPSPlayerState.cpp
@@ -1,1 +1,16 @@
 #include "GASMultiFPS/Public/Player/PlayerState/GASMultiFPSPlayerState.h"
+
+#include "GASMultiFPS/Public/AbilitySysComp/GASMultiFPSAbilitySystemComponent.h"
+
+
+AGASMultiFPSPlayerState::AGASMultiFPSPlayerState()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UGASMultiFPSAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+}
+
+UAbilitySystemComponent* AGASMultiFPSPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
--- Source/GASMultiFPS/Public/Player/PlayerState/GASMultiFPSPlayerState.h
+++ Source/GASMultiFPS/Public/Player/PlayerState/GASMultiFPSPlayerState.h
@@ -1,11 +1,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/PlayerState.h"
 #include "GASMultiFPSPlayerState.generated.h"
 
+class UGASMultiFPSAbilitySystemComponent;
+
 UCLASS()
-class GASMULTIFPS_API AGASMultiFPSPlayerState : public APlayerState
+class GASMULTIFPS_API AGASMultiFPSPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
+	
+public:
+	AGASMultiFPSPlayerState();
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+#pragma region ASCGetter
+	UGASMultiFPSAbilitySystemComponent* GetGASMultiFPSASC() const {return AbilitySystemComponent;}
+#pragma endregion 
+	
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UGASMultiFPSAbilitySystemComponent> AbilitySystemComponent;
 };
