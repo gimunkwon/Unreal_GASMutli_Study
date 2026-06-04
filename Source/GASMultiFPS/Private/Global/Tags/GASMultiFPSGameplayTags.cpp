57449e9e1649fe76be9b1d9b85b4140bcdf09a41
gimunkwon <gimon71@gmail.com>
2026-06-04 23:33:22 +0900

    [Add] GamePlayTags

---
+++ Source/GASMultiFPS/Private/Global/Tags/GASMultiFPSGameplayTags.cpp
@@ -0,0 +1,12 @@
+#include "GASMultiFPS/Public/Global/Tags/GASMultiFPSGameplayTags.h"
+
+namespace GASMultiFPSTags
+{
+	// Input
+	UE_DEFINE_GAMEPLAY_TAG(Input_Fire, "Input.Fire")
+	UE_DEFINE_GAMEPLAY_TAG(Input_Reload, "Input.Reload")
+	// State
+	UE_DEFINE_GAMEPLAY_TAG(State_Dead, "State.Dead")
+	UE_DEFINE_GAMEPLAY_TAG(State_Firing, "State.Firing")
+	UE_DEFINE_GAMEPLAY_TAG(State_Reloading, "State.Reloading")
+}
---
+++ Source/GASMultiFPS/Public/Global/Tags/GASMultiFPSGameplayTags.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "NativeGameplayTags.h" 
+
+namespace GASMultiFPSTags
+{
+	// Input
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Fire)
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Reload)
+	// State
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Dead)
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Firing)
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Reloading)
+}
