559087b64645d3bf8d96b93e847e93097fdebded
gimunkwon <gimon71@gmail.com>
2026-06-04 22:51:41 +0900

    [Add] GAS 플러그인 추가및 모듈 추가

--- Source/GASMultiFPS/GASMultiFPS.Build.cs
+++ Source/GASMultiFPS/GASMultiFPS.Build.cs
@@ -8,7 +8,11 @@ public class GASMultiFPS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"
+			,"GameplayAbilities","GameplayTags","GameplayTasks"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
