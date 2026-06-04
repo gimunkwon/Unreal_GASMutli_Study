706ddbf94bef2cd47db7d3c60d67233218530517
gimunkwon <gimon71@gmail.com>
2026-06-04 23:26:32 +0900

    [Add,Feature] ASC Getter 메서드 로직 정의 PossessedBy(Server) 에서 어빌리티 시스템 초기화 OnRep_PlayerState(Client)에서 어빌리티 시스템 초기화

--- Source/GASMultiFPS/Private/Player/Character/GASMultiFPSCharacter.cpp
+++ Source/GASMultiFPS/Private/Player/Character/GASMultiFPSCharacter.cpp
@@ -1,5 +1,7 @@
 #include "GASMultiFPS/Public/Player/Character/GASMultiFPSCharacter.h"
 
+#include "AbilitySystemComponent.h"
+#include "GASMultiFPS/Public/Player/PlayerState/GASMultiFPSPlayerState.h"
 
 
 AGASMultiFPSCharacter::AGASMultiFPSCharacter()
@@ -7,22 +9,36 @@ AGASMultiFPSCharacter::AGASMultiFPSCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 }
 
-
 void AGASMultiFPSCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+UAbilitySystemComponent* AGASMultiFPSCharacter::GetAbilitySystemComponent() const
+{
+	const AGASMultiFPSPlayerState* PS = GetPlayerState<AGASMultiFPSPlayerState>();
 	
+	return PS ? PS->GetAbilitySystemComponent() : nullptr;
 }
 
-
-void AGASMultiFPSCharacter::Tick(float DeltaTime)
+void AGASMultiFPSCharacter::PossessedBy(AController* NewController)
 {
-	Super::Tick(DeltaTime);
+	Super::PossessedBy(NewController);
+	
+	InitAbilitySystem();
 }
 
-
-void AGASMultiFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AGASMultiFPSCharacter::OnRep_PlayerState()
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::OnRep_PlayerState();
+	
+	InitAbilitySystem();
 }
 
+void AGASMultiFPSCharacter::InitAbilitySystem()
+{
+	AGASMultiFPSPlayerState* PS = GetPlayerState<AGASMultiFPSPlayerState>();
+	if (!PS) return;
+	
+	PS->GetAbilitySystemComponent()->InitAbilityActorInfo(PS,this);
+}
--- Source/GASMultiFPS/Public/Player/Character/GASMultiFPSCharacter.h
+++ Source/GASMultiFPS/Public/Player/Character/GASMultiFPSCharacter.h
@@ -1,19 +1,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
 #include "GASMultiFPSCharacter.generated.h"
 
 UCLASS()
-class GASMULTIFPS_API AGASMultiFPSCharacter : public ACharacter
+class GASMULTIFPS_API AGASMultiFPSCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AGASMultiFPSCharacter();
+	
+#pragma region ASCGetter
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+#pragma endregion 
+	
 protected:
 	virtual void BeginPlay() override;
-public:
-	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// Server
+	virtual void PossessedBy(AController* NewController) override;
+	// Client
+	virtual void OnRep_PlayerState() override;
+private:
+	void InitAbilitySystem();
 };
